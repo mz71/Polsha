8620d6ac27bcdca000139a05304472f1eb1a4dc4
mz71 <m_zugaj@outlook.com>
2020-08-31 19:52:51 +0200

    initial commit

---
+++ PolshaCompiler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PolshaCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write(">");
+                var line = Console.ReadLine();
+                 
+            }
+        }
+    }
+}
